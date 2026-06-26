2e77cbebb4223dd5ff4d0bf1f43d40866733b8fa
Vivica Moreno <michael.kargl@posteo.net>
2026-06-26 16:28:12 +0200

    chore: removes third party reference in osaHealth.Framework

--- src/osaHealth.Framework/Api.fs
+++ src/osaHealth.Framework/Api.fs
@@ -5,44 +5,27 @@ open System.Net
 open System.Net.Http
 open System.Text
 open System.Threading.Tasks
-open FSharp.UMX
 open osaHealth.Framework.Json
 
-[<Measure>]
-type HttpResponse
-
-[<Measure>]
-type HttpEndpoint
-
-[<Measure>]
-type HttpPath
-
-[<Measure>]
-type HttpBody
-
-let buildHttpClient (endpoint: string<HttpEndpoint>) =
-    new HttpClient(BaseAddress = Uri(endpoint |> UMX.untag))
+let buildHttpClient (endpoint: string) =
+    new HttpClient(BaseAddress = Uri(endpoint))
 
 let postQueryAsync
-    (endpoint: string<HttpEndpoint>)
-    (path: string<HttpPath>)
-    (body: string<HttpBody>)
-    : Task<HttpStatusCode * string<HttpResponse>> =
+    (endpoint: string)
+    (path: string)
+    (body: string)
+    : Task<HttpStatusCode * string> =
     task {
-        let body = body |> UMX.untag
-        let path = path |> UMX.untag
         let client = buildHttpClient endpoint
         let content = new StringContent(body, Encoding.UTF8, "application/json")
         let! response = client.PostAsync(path, content)
         let! responseText = response.Content.ReadAsStringAsync()
-        return response.StatusCode, (responseText.Trim() |> UMX.tag)
+        return response.StatusCode, responseText.Trim()
     }
 
-let displayResponse (label: string) (statusCode: HttpStatusCode, body: string<HttpResponse>) : unit =
-    let body = body |> UMX.untag
-
-    printfn "--- %s ---" label
-    printfn "HTTP %A" statusCode
+let displayResponse (label: string) (statusCode: HttpStatusCode, body: string) : unit =
+    printfn $"--- %s{label} ---"
+    printfn $"HTTP %A{statusCode}"
 
     if body = "" then
         printfn "(empty body)\n"
